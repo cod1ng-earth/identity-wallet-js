9feae75f56edd64a007b847ddf2816d3da49fa8c
Joel Torstensson <oed3933@gmail.com>
2020-04-04 10:30:13 +0200

    feat: more management key operations

--- src/__tests__/identity-wallet.test.js
+++ src/__tests__/identity-wallet.test.js
@@ -1,7 +1,7 @@
 const IdentityWallet = require('../identity-wallet')
 const HDNode = require('@ethersproject/hdnode')
-const { decodeJWT, verifyJWT } = require('did-jwt')
-const { registerMethod } = require('did-resolver')
+const { verifyJWT } = require('did-jwt')
+const { Resolver } = require('did-resolver')
 
 const wallet1Conf = {
   seed: HDNode.mnemonicToSeed('clay rubber drama brush salute cream nerve wear stuff sentence trade conduct')
@@ -16,29 +16,58 @@ const badAuthData = [{
 }]
 const getConsentMock = jest.fn(() => false)
 
-registerMethod('3', async (_, { id }) => {
-  let key
-  if (id === 'bafyreia6evyez2xdlewmbh7hfz3dz3besmlhnlrnkiounscnnvboym7q2u' || id === 'first') {
-    key = '027ab5238257532f486cbeeac59a5721bbfec2f13c3d26516ca9d4c5f0ec1aa229'
-  } else {
-    // key for 'space1'
-    key = '0283441873077702f08a9e84d0ff869b5d08cb37361d77c7e5c57777e953670a0d'
-  }
-  return {
-    '@context': 'https://w3id.org/did/v1',
-    'id': 'did:3:' + id,
-    'publicKey': [{
-      'id': 'did:3:' + id + '#owner',
-      'type': 'Secp256k1VerificationKey2018',
-      'owner': 'did:3:' + id,
-      'publicKeyHex': key
-    }],
-    'authentication': [{
-      'type': 'Secp256k1SignatureAuthentication2018',
-      'publicKey': 'did:3:' + id + '#owner'
-    }]
+
+const didResolver = new Resolver({
+  '3': async (_, { id }) => {
+    let key
+    if (id === 'bafyreia6evyez2xdlewmbh7hfz3dz3besmlhnlrnkiounscnnvboym7q2u' || id === 'first') {
+      key = '027ab5238257532f486cbeeac59a5721bbfec2f13c3d26516ca9d4c5f0ec1aa229'
+    } else {
+      // key for 'space1'
+      key = '0283441873077702f08a9e84d0ff869b5d08cb37361d77c7e5c57777e953670a0d'
+    }
+    return {
+      '@context': 'https://w3id.org/did/v1',
+      'id': 'did:3:' + id,
+      'publicKey': [{
+        'id': 'did:3:' + id + '#owner',
+        'type': 'Secp256k1VerificationKey2018',
+        'owner': 'did:3:' + id,
+        'publicKeyHex': key
+      }],
+      'authentication': [{
+        'type': 'Secp256k1SignatureAuthentication2018',
+        'publicKey': 'did:3:' + id + '#owner'
+      }]
+    }
   }
 })
+
+const verifyJWTOpts = { resolver: didResolver }
+
+//registerMethod('3', async (_, { id }) => {
+  //let key
+  //if (id === 'bafyreia6evyez2xdlewmbh7hfz3dz3besmlhnlrnkiounscnnvboym7q2u' || id === 'first') {
+    //key = '027ab5238257532f486cbeeac59a5721bbfec2f13c3d26516ca9d4c5f0ec1aa229'
+  //} else {
+    //// key for 'space1'
+    //key = '0283441873077702f08a9e84d0ff869b5d08cb37361d77c7e5c57777e953670a0d'
+  //}
+  //return {
+    //'@context': 'https://w3id.org/did/v1',
+    //'id': 'did:3:' + id,
+    //'publicKey': [{
+      //'id': 'did:3:' + id + '#owner',
+      //'type': 'Secp256k1VerificationKey2018',
+      //'owner': 'did:3:' + id,
+      //'publicKeyHex': key
+    //}],
+    //'authentication': [{
+      //'type': 'Secp256k1SignatureAuthentication2018',
+      //'publicKey': 'did:3:' + id + '#owner'
+    //}]
+  //}
+//})
 
 
 describe('IdentityWallet', () => {
@@ -185,9 +214,9 @@ describe('IdentityWallet', () => {
     const jwt1 = await idWallet1.signClaim(payload, { DID: 'did:3:first' })
     const jwt2 = await idWallet1.signClaim(payload, { space: 'space1' })
 
-    expect(await verifyJWT(jwt0)).toBeDefined()
-    expect(await verifyJWT(jwt1)).toBeDefined()
-    expect(await verifyJWT(jwt2)).toBeDefined()
+    expect(await verifyJWT(jwt0, verifyJWTOpts)).toBeDefined()
+    expect(await verifyJWT(jwt1, verifyJWTOpts)).toBeDefined()
+    expect(await verifyJWT(jwt2, verifyJWTOpts)).toBeDefined()
   })
 
   it('encrypt/decrypt works correctly', async () => {
